4d50d858b29e1b4996305600fa27c0d7bfd8bc67
Zaman Sheikh <zaman6545@gmail.com>
2022-10-18 09:09:22 +0600

    new file:   Insertion_Sort.c 	new file:   Merge_Sort.c 	new file:   Quick_Sort.c 	new file:   Selection_Sort.c

---
+++ Insertion_Sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *x, int *y);
+void insertion_sort(int a[], int length);
+
+int main()
+{
+    int a[] = {10, 11, 23, 44, 8, 15, 3, 9, 12, 45, 56, 45, 45};
+    int length = sizeof(a) / sizeof(a[0]);
+    insertion_sort(a, length);
+    printf("This is the sorted array: ");
+    for (int i = 0; i < length; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void insertion_sort(int a[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        int smallest;
+        for (int j = 1; j < length; j++)
+        {
+            if (a[j] < smallest)
+                smallest = a[j];
+        }
+        swap(smallest, a[0]);
+    }
+}
