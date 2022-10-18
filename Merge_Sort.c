d7994e63e8f175847309bb94c7ce88dcc2f74104
Zaman Sheikh <zaman6545@gmail.com>
2022-10-18 20:50:15 +0600

    MergeSort

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-void swap(int *x, int *y);
-void insertion_sort(int a[], int length);
+void merge_sort(int a[], int l, int r);
+void merge_arrays(int a[], int l, int m, int r);
 
 int main()
 {
-    int a[] = {10,11,23,44,8,15,3,9,12,45,56,45,45};
-    int length = sizeof(a)/sizeof(a[0]);
-    insertion_sort(a, length);
-    printf("This is the sorted array: ");
-    for( int i = 0; i < length; i++)
-        printf("%d ", a[i]);
-    return 0;
+  int array[] = { 9, 4, 8, 1, 7, 0, 3, 2, 5, 6};
+  int length = sizeof(array) / sizeof(array[0]);
+  merge_sort(array, 0, length - 1);
+  for (int i = 0; i < length; i++)
+    printf("%d ", array[i]);
+  printf("\n");
+  return 0;
 }
 
-void swap(int *x, int *y)
-{
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+void merge_sort(int a[], int l, int r){
+    if ( l < r){
+        int mid = l + (r - l)/2;
+        merge_sort(a, l, mid);
+        merge_sort(a, mid+1, r);
+        merge_arrays(a, l, mid, r);
+    }
+}
+void merge_arrays(int a[], int l, int m, int r){
+    int l_lenth = m - l + 1;
+    int r_length = r - m;
+    int temp_l[l_lenth], temp_r[r_length];
+    int i, j, k;
+    for (i = 0; i < l_lenth; i++)
+        temp_l[i] = a[l + i];
+
+    for (j = 0; j < r_length; j++)
+        temp_r[j] = a[m + 1 + j];
+
+    for (i = 0, j = 0, k = l; k <= r ; k++){
+        if ( i < l_lenth &&
+              (j >= r_length || temp_l[i] <= temp_r[j]))
+            a[k] = temp_l[i++];
+        else
+            a[k] = temp_r[j++];
+    }
 }
