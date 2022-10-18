6a388fa748c835794cad70d2156b23bb1c2f79f7
Zaman Sheikh <zaman6545@gmail.com>
2022-10-18 08:26:08 +0600

    BubbleSortingAdded

---
+++ Bubble_Sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *x, int *y);
+void bubble_sort(int a[], int length);
+int main()
+{
+    int a[] = {10,11,23,44,8,15,3,9,12,45,56,45,45};
+    int length = sizeof(a)/sizeof(a[0]);
+    bubble_sort(a, length);
+
+    for (int i = 0; i < length; i++)
+        printf("%d ", a[i]);
+    
+
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubble_sort(int a[], int length){
+    bool swapped;
+    do{
+        int i = 0;
+        swapped = false;
+        for ( int j = 0; j < length - 1 - i ; j ++){
+            if( a[j] > a[j+1]){
+                swap(&a[j], &a[j+1]);
+                swapped = true; 
+            }
+        }
+        i++;
+    }while(swapped);
+}
