f7da00d5a60497b3c2a1307c8954517ec244d09e
Zaman Sheikh <zaman6545@gmail.com>
2022-10-18 19:25:04 +0600

    modified:   Selection_Sort.c 	new file:   Selection_Sort.exe

--- Selection_Sort.c
+++ Selection_Sort.c
@@ -2,15 +2,15 @@
 #include <stdbool.h>
 
 void swap(int *x, int *y);
-void insertion_sort(int a[], int length);
+void selection_sort(int a[], int length);
 
 int main()
 {
-    int a[] = {10,11,23,44,8,15,3,9,12,45,56,45,45};
-    int length = sizeof(a)/sizeof(a[0]);
-    insertion_sort(a, length);
+    int a[] = {10, 11, 23, 44, 8, 15, 3, 9, 12, 45, 56, 45, 45};
+    int length = sizeof(a) / sizeof(a[0]);
+    selection_sort(a, length);
     printf("This is the sorted array: ");
-    for( int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
         printf("%d ", a[i]);
     return 0;
 }
@@ -20,4 +20,15 @@ void swap(int *x, int *y)
     int temp = *x;
     *x = *y;
     *y = temp;
+}
+
+void selection_sort(int a[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        int minimum_pos = i;
+        for (int j = i + 1; j < length; j++)
+            if (a[j] < a[minimum_pos]) minimum_pos = j;
+        swap(&a[i], &a[minimum_pos]);
+    }
 }
